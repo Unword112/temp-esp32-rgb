8c5762a51e1e71f7a418e10fa64887ee346d6220
Unword112 <pog123@hotmail.co.uk>
2025-03-05 02:43:03 +0700

    done

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,14 @@
 
 const char* ssid = "Wokwi-GUEST";
 const char* password = "";
+
 const char* mqtt_server = "demo.thingsboard.io";
 const char* ID = "2da86770-f6a6-11ef-8e00-e370a74757c3";
 const char* token = "eN7Ny6EDpSzARiDrxU9E";
 const int port = 1883;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
 
 #define R 5
 #define G 4
@@ -18,28 +22,39 @@ const int port = 1883;
 #define DHTTYPE DHT22
 DHT dht(DHTPIN, DHTTYPE);
 
-#define TEMP_MIN 17.22  
-#define TEMP_MAX 36.11  
-
-WiFiClient espClient;
-PubSubClient client(espClient);
-
-void setup_wifi() {
-  Serial.print("Connecting to WiFi...");
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.print(".");
-  }
-  Serial.println("Connected!");
-}
+float TEMP_MIN = 0;
+float TEMP_MAX = 0;
 
 void reconnect() {
   while (!client.connected()) {
     if (client.connect(ID, token, "")) {
-      Serial.println("Connected to MQTT Broker");
+      client.subscribe("v1/devices/me/rpc/request/+"); // Subscribe RPC
     } else {
       delay(5000);
+    }
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  String message;
+  for (unsigned int i = 0; i < length; i++) {
+    message += (char)payload[i];
+  }
+  Serial.println("Received: " + message);
+
+  if (message.indexOf("\"method\":\"setTempMin\"") != -1) {
+    int paramsMinIndex = message.indexOf("\"params\":");
+    if (paramsMinIndex != -1) {
+      int valueStart = message.indexOf(":", paramsMinIndex) + 1;
+      TEMP_MIN = message.substring(valueStart, message.indexOf("}", paramsMinIndex)).toFloat();
+      Serial.printf("Updated Temp Min: %.2f\n", TEMP_MIN);
+    }
+  } else if (message.indexOf("\"method\":\"setTempMax\"") != -1) {
+    int paramsMaxIndex = message.indexOf("\"params\":");
+    if (paramsMaxIndex != -1) {
+      int valueStart = message.indexOf(":", paramsMaxIndex) + 1;
+      TEMP_MAX = message.substring(valueStart, message.indexOf("}", paramsMaxIndex)).toFloat();
+      Serial.printf("Updated Temp Max: %.2f\n", TEMP_MAX);
     }
   }
 }
@@ -54,17 +69,32 @@ void sendTemperature(float temp, String colorHex) {
   String payload = "{\"temperature\": " + String(temp, 1) + ", \"led_color\": \"" + colorHex + "\"}";
   client.publish("v1/devices/me/telemetry", payload.c_str());
   Serial.println("Sent to ThingsBoard: " + payload);
+  delay(5000);
 }
+
 
 void setup() {
   Serial.begin(115200);
-  setup_wifi();
+  
+  Serial.print("Connecting to WiFi...");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.println("Connected!");
+
   client.setServer(mqtt_server, port);
+  client.setCallback(callback);
+
   dht.begin();
+
+  reconnect();
 
   pinMode(R, OUTPUT);
   pinMode(G, OUTPUT);
   pinMode(B, OUTPUT);
+
 }
 
 void loop() {
@@ -88,6 +118,7 @@ void loop() {
   }
 
   sendTemperature(temp, colorHex);
+  Serial.printf("TEMP_MIN: %.2f, TEMP_MAX: %.2f\n", TEMP_MIN, TEMP_MAX);
 
-  delay(5000); 
+  delay(500);
 }
